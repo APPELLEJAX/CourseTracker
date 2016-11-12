fe697af648868ce5d50d190deb9ad44a3e7168e9
APPELLEJAX <midnbeal18@gmail.com>
2016-11-11 23:54:52 -0500

    commit

---
+++ page.inc.php
@@ -0,0 +1,28 @@
+<?php
+
+  class Page{
+    $title = "CourseTracker";
+    $stylesheet = "";
+    $content = "";
+
+    function __construct($title){
+      $this->title .= " - " . $title;
+    }
+
+    function displayMax(){
+
+    }
+
+    function dipslayMin(){
+
+    }
+
+    function displayTop(){
+      echo "<!DOCTYPE html>"
+    }
+
+
+
+  }
+
+ ?>
